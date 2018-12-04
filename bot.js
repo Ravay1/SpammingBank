d941e4d02218334babdb01737e05879812b99e93
ELMaestro252 <45597115+ELMaestro252@users.noreply.github.com>
2018-12-04 15:37:15 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("519498723558948875")
+setInterval(function() {
+channel.send(`$$$,$$$,$$$,$$$,$$$,$$$,$$$,$$$,$$$,$$$ MSBANK`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
